8633fc49213c2d7980df19835367f7df37374b2f
Urfer Leila <leila.urfer@ceff.ch>
2026-04-02 08:04:43 +0200

    Finished design

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,13 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id_user';
+
+    /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,45 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vos Jobs - Job Dispatcher</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="div-login">
-    <x-header/>
-    <div class="p-10">
-        <div class="div2-home" style="position: relative;">
-            <h2>Vos jobs</h2>
-            <a href="{{ route('jobs.create') }}" class="btn-add" style="position: absolute; right: 0;">Ajouter un job</a>
-        </div>
-        <div class="flex justify-around mb-16 text-2xl">
-            <div class="flex items-center gap-4">Terminé : <span class="status-circle bg-green"></span></div>
-            <div class="flex items-center gap-4">En cours : <span class="status-circle bg-orange"></span></div>
-            <div class="flex items-center gap-4">En attente : <span class="status-circle bg-red"></span></div>
-        </div>
-        <div class="max-w-7xl mx-auto">
-            <div class="task-card">
-                <div class="task-info">
-                    <span>Tâche: 1</span>
-                    <span>Fichier : fichier_a_imprimer</span>
+    <div class="div-login min-h-screen">
+        <x-header/>
+        <div class="p-10">
+            <div class="div2-home">
+                <div class="flex-1"></div> <h2>Vos jobs</h2>
+                <div class="flex-1 flex items-center justify-end gap-6">
+                    <a href="{{ route('jobs.create') }}" class="btn-add">Ajouter un job</a>
                 </div>
-                <div class="flex items-center gap-6">Status : <span class="status-circle bg-green"></span></div>
             </div>
-            <div class="task-card">
-                <div class="task-info">
-                    <span>Tâche: 1</span>
-                    <span>Fichier : fichier_a_imprimer</span>
-                </div>
-                <div class="flex items-center gap-6">Status : <span class="status-circle bg-orange"></span></div>
+            <div class="flex justify-around mb-16 text-2xl mt-12">
+                <div class="flex items-center gap-4">Terminé : <span class="status-circle bg-green"></span></div>
+                <div class="flex items-center gap-4">En cours : <span class="status-circle bg-orange"></span></div>
+                <div class="flex items-center gap-4">En attente : <span class="status-circle bg-red"></span></div>
             </div>
-            <div class="task-card">
-                <div class="task-info">
-                    <span>Tâche: 1</span>
-                    <span>Fichier : fichier_a_imprimer</span>
+            <div class="max-w-5xl mx-auto space-y-6">
+                <div class="task-card">
+                    <div class="task-info">
+                        <span>Tâche: 1</span>
+                        <span>Fichier : fichier_a_imprimer</span>
+                    </div>
+                    <div class="flex items-center gap-6">
+                        <span>Status :</span>
+                        <span class="status-circle bg-green"></span>
+                    </div>
                 </div>
-                <div class="flex items-center gap-6">Status : <span class="status-circle bg-red"></span></div>
+                <div class="task-card">
+                    <div class="task-info">
+                        <span>Tâche: 2</span>
+                        <span>Fichier : fichier_a_imprimer</span>
+                    </div>
+                    <div class="flex items-center gap-6">
+                        <span>Status :</span>
+                        <span class="status-circle bg-orange"></span>
+                    </div>
+                </div>
+                <div class="task-card">
+                    <div class="task-info">
+                        <span>Tâche: 3</span>
+                        <span>Fichier : fichier_a_imprimer</span>
+                    </div>
+                    <div class="flex items-center gap-6">
+                        <span>Status :</span>
+                        <span class="status-circle bg-red"></span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </body>
 </html>
